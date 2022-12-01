f1b33be1b07dd40bdc278db5aabab3b38447db0b
Arthur Romansini Fernandes <arthurromansinif@gmail.com>
2022-12-01 10:32:30 -0300

    Alterar Manutencao
    funcao tamanho array, e funcao de id do array de acordo com a escolha do menu

--- app/alterarManutencao.py
+++ app/alterarManutencao.py
@@ -4,6 +4,8 @@ sys.path.append('./')
 import database.createTables
 from utilities.formatQuery import formatQuery
 from utilities.Menu import menu
+from utilities.checaTamanho import checa_tamanho
+from utilities.checaTamanho import retorna_id
 import sqlite3
 
 
@@ -22,8 +24,14 @@ def altera_manutencao():
         
     query = conn.execute(
             'SELECT id, nome, detalhe, tipoVeiculo, valor, descricao FROM manutencao WHERE cpf = {0}'.format(cpfPesquisa))        
-  
-    id = int(menu(query.fetchall(), nome, "Selecione o id da manutenção que deseja alterar(Primeiro valor): "))
+
+    listaManutencoesCpf = query.fetchall()
+
+    if checa_tamanho(listaManutencoesCpf):
+        pos = int(menu(listaManutencoesCpf, nome))
+        id = retorna_id(listaManutencoesCpf, pos)
+    else:
+        id = retorna_id(listaManutencoesCpf, 1)
 
     query = conn.execute(
             'SELECT detalhe, tipoVeiculo, valor, descricao, nome FROM manutencao WHERE id = {0}'.format(id))
@@ -32,7 +40,7 @@ def altera_manutencao():
         print(i)
 
     escolha_id = ['Detalhe', 'Tipo de veiculo','Descrição', 'Valor', 'Nome']
-    escolha = int(menu(escolha_id, nome, 'Escolha o que deseja alterar:'))
+    escolha = int(menu(escolha_id, nome, 'Escolha o que deseja alterar: '))
 
     valores = {}
 
@@ -42,24 +50,28 @@ def altera_manutencao():
                 valores['detalhe'], id))
             conn.commit()
             print('Detalhe alterado com sucesso!')
+
     elif (escolha == 2):
             valores['tipoVeiculo'] = input("Digite o novo tipo de veiculo: ")
             query = conn.execute('''UPDATE manutencao SET tipoVeiculo = '{0}' WHERE id = {1}'''.format(
                 valores['tipoVeiculo'], id))
             conn.commit()
             print("Tipo de veiculo alterado com sucesso!")
+
     elif (escolha == 3):
             valores['valor'] = input("Digite o novo valor: ")
             query = conn.execute('''UPDATE manutencao SET valor = '{0}' WHERE id = {1}'''.format(
                 valores['valor'], id))
             conn.commit()
             print("Valor alterado com sucesso!")
+
     elif (escolha == 4):
             valores['descricao'] = input("Digite a nova descrição: ")
             query = conn.execute('''UPDATE manutencao SET descricao = '{0}' WHERE id = {1}'''.format(
                 valores['descricao'], id))
             conn.commit()
             print("Descrição alterada com sucesso!")
+
     elif (escolha == 5):
             valores['nome'] = input("Digite o novo nome: ")
             query = conn.execute('''UPDATE manutencao SET nome = '{0}' WHERE id = {1}'''.format(
---
+++ utilities/checaTamanho.py
@@ -0,0 +1,15 @@
+def retorna_id( query, posicao ):
+    for a in query:
+        if (query.index(a)+1) == posicao: 
+            b = a     
+    return b[0]
+
+def checa_tamanho( query ):
+    i = 0
+    for a in query:
+        i += 1
+
+    if i == 1:
+        return False
+    else:
+        return True

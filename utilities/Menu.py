0eb89a82908a78865f04772834fc949ee1bfe8d0
Arthur Romansini Fernandes <arthurromansinif@gmail.com>
2022-12-08 08:35:53 -0300

    Comentarios e alterar manutenção

--- utilities/Menu.py
+++ utilities/Menu.py
@@ -1,12 +1,20 @@
+
+#Recebe como argumentos: Opções do menu, Nome do Menu e o texto presente no input ao fim do menu.
 def menu(op = [], nome = "menu", selecao = 'Digite uma das opções: '):
     pontos = "-----"
 
     print(pontos, nome, pontos)
 
+    #Utiliza um if para verificar se existe alguma opção
+    #e usa um laço de repetição para percorrer o array de opções
+    #colocando o numero de cada opção e formatando no formato
+    # Index - Opção.
     if len(op) != 0:        
         for i in op:
             intro = [(op.index(i)+1), "-"]
             print(intro[0],intro[1], i)
     
+    #Utiliza o input no final do menu, caso não passe nenhum 
+    #input como argumento ele coloca por padrão uma mensagem genérica
     opcao = input(selecao)
     return opcao
--- utilities/checaTamanho.py
+++ utilities/checaTamanho.py
@@ -1,9 +1,14 @@
+#Recebe e query e a posição escolhida da linha
+# após isso percorre o array e devolve a opção de ID da linha 
 def retorna_id( query, posicao ):
     for a in query:
         if (query.index(a)+1) == posicao: 
             b = a     
     return b[0]
 
+
+#Recebe como parametro o cursor da query do banco de dados
+# e retorna a quantidade de linhas presentes no array
 def checa_tamanho( query ):
     i = 0
     for a in query:
